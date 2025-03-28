2ee90398d3ea915061e2c6cea24470d62ef4f718
jisaa_yoga <giuseppe.richetti@gmail.com>
2025-03-28 12:53:17 +0100

    Add GetMarshaledKey function

--- awsops_dynamo.go
+++ awsops_dynamo.go
@@ -1,11 +1,29 @@
 package awsops
 
 import (
+	"log"
 
+	"github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
 )
 
 // InitDynamoDBClient creates a new DynamoDBClient
 func (a *AWSOps) InitDynamoDBClient() {
    a.DynamoDBClient = dynamodb.NewFromConfig(a.cfg.AWSConfig)
 }
+
+// GetMarshaledKey returns a key ready to be used for dynamoDB operations. 
+// Every database operation that needs an ItemInput{} requires a key in the map[string]types.AttributeValue{} format.
+// Exits with Fatalf if marshaling is unsuccessful.
+//
+// E.g. 
+// dynamodb.DeleteItemInput{Key: GetMarshaledKey("myKeyValue", "myKeyName")} is needed by the dynamodb.Client.DeleteItem() operation.
+func GetMarshaledKey(keyValue, keyName string) map[string]types.AttributeValue {
+    marshaledKey, err := attributevalue.Marshal(keyValue)
+    if err != nil {
+        log.Fatalf("\nerror marshaling %s key. Here's why: %v", err)
+    }
+    return map[string]types.AttributeValue{keyName: marshaledKey}
+}
+
--- go.mod
+++ go.mod
@@ -11,11 +11,13 @@ require (
 	github.com/aws/aws-sdk-go-v2/aws/protocol/eventstream v1.6.10 // indirect
 	github.com/aws/aws-sdk-go-v2/config v1.29.11 // indirect
 	github.com/aws/aws-sdk-go-v2/credentials v1.17.64 // indirect
+	github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue v1.18.8 // indirect
 	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.16.30 // indirect
 	github.com/aws/aws-sdk-go-v2/internal/configsources v1.3.34 // indirect
 	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.6.34 // indirect
 	github.com/aws/aws-sdk-go-v2/internal/ini v1.8.3 // indirect
 	github.com/aws/aws-sdk-go-v2/internal/v4a v1.3.34 // indirect
+	github.com/aws/aws-sdk-go-v2/service/dynamodbstreams v1.25.1 // indirect
 	github.com/aws/aws-sdk-go-v2/service/internal/accept-encoding v1.12.3 // indirect
 	github.com/aws/aws-sdk-go-v2/service/internal/checksum v1.7.0 // indirect
 	github.com/aws/aws-sdk-go-v2/service/internal/endpoint-discovery v1.10.15 // indirect
