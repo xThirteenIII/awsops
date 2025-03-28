7c6f3d0daacc3607a16059d12ada48c5fca0b131
jisaa_yoga <giuseppe.richetti@gmail.com>
2025-03-28 12:15:22 +0100

    Add initConfig method

--- awsops.go
+++ awsops.go
@@ -1,23 +1,56 @@
 package awsops
 
 import (
+	"context"
+	"fmt"
+
 	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
 )
 
 // AWSOps provides AWS configurations along with AWS services clients.
 type AWSOps struct {
-    Config          aws.Config
+    cfg             Config //Not a pointer, we don't want it to be modified.
     DynamoDBClient  *dynamodb.Client
 }
 
-// NewAWSOps returns a new empty AWSOps pointer.
-func NewAWSOps() *AWSOps {
-    return &AWSOps{}
+type Config struct {
+    AWSConfig  aws.Config
+}
+
+// NewAWSOps returns a new AWSOps pointer.
+// The AWSOps structure is initialized.
+//
+// Returns the error encountered by initConfig.
+func NewAWSOps() (*AWSOps, error) {
+
+    conf, err := initConfig(context.TODO())
+    if err != nil {
+        return &AWSOps{}, nil
+    }
+    
+    return &AWSOps{cfg: conf}, nil
 }
 
 // Copy returns a shallow copy of the AWSOps object.
 func (a AWSOps) Copy() AWSOps {
     cp := a
     return cp
+}
+
+// initConfig is the private function called by NewAWSOps.
+//
+// It returns the error encountered by the config.LoadDefaultConfig official AWS function.
+// Optional configs are not supported yet.
+func initConfig(ctx context.Context) (Config, error) {
+
+    fmt.Printf("\nLoading AWS configurations... ")
+    cfg, err := config.LoadDefaultConfig(ctx)
+    if err != nil {
+        return Config{}, fmt.Errorf("failed loading aws configurations. Here's why: %v", err)
+    }
+    fmt.Printf("Done\n")
+
+    return Config{AWSConfig: cfg}, nil
 }
--- go.mod
+++ go.mod
@@ -9,8 +9,12 @@ require (
 
 require (
 	github.com/aws/aws-sdk-go-v2/aws/protocol/eventstream v1.6.10 // indirect
+	github.com/aws/aws-sdk-go-v2/config v1.29.11 // indirect
+	github.com/aws/aws-sdk-go-v2/credentials v1.17.64 // indirect
+	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.16.30 // indirect
 	github.com/aws/aws-sdk-go-v2/internal/configsources v1.3.34 // indirect
 	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.6.34 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/ini v1.8.3 // indirect
 	github.com/aws/aws-sdk-go-v2/internal/v4a v1.3.34 // indirect
 	github.com/aws/aws-sdk-go-v2/service/internal/accept-encoding v1.12.3 // indirect
 	github.com/aws/aws-sdk-go-v2/service/internal/checksum v1.7.0 // indirect
@@ -20,5 +24,8 @@ require (
 	github.com/aws/aws-sdk-go-v2/service/iot v1.64.1 // indirect
 	github.com/aws/aws-sdk-go-v2/service/s3 v1.78.2 // indirect
 	github.com/aws/aws-sdk-go-v2/service/sqs v1.38.2 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sso v1.25.2 // indirect
+	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.29.2 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sts v1.33.17 // indirect
 	github.com/aws/smithy-go v1.22.2 // indirect
 )
